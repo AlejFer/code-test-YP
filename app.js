56cf6a551827233d137d4b7f75d3cc017eb27bc6
Luis Fer Alej <luis.alefg@gmail.com>
2019-10-17 12:02:22 +0200

    App listening

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const url = "mongodb://localhost:27017";
 const express = require('express');
 const bodyParse = require('body-parser');
 const mongodb = require('mongodb');
+
+//Init
+const app = express();
+
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
+
+app.get('/', (req, res) => {
+    console.log("Request recieved from: " + (req.header['x-forwarded-for'] || req.connection.remoteAddress));
+    res.send("Hi " + (req.header['x-forwarded-for'] || req.connection.remoteAddress));
+});
+
+app.listen(port, () => {
+    console.log(`Listening to htp://localhost:${port}/`);
+});
