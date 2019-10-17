79becd6e68cc8f834b1ad42b524ca32151685e4f
Luis Fer Alej <luis.alefg@gmail.com>
2019-10-17 11:52:37 +0200

    App.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const port = 9090;
+const url = "mongodb://localhost:27017";
+// Add Dependencies
+const express = require('express');
+const bodyParse = require('body-parser');
+const mongodb = require('mongodb');
