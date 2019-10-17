376d6908f1d6755a8c4f81754d31b08512798e96
Luis Fer Alej <luis.alefg@gmail.com>
2019-10-17 13:00:22 +0200

    Conneting to MongoDB, get movies / add movie

--- app.js
+++ app.js
@@ -13,20 +13,65 @@ app.use(bodyParse.json());
 
 // Routes
 app.get('/', (req, res) => {
-    console.log("Request recieved from: " + (req.header['x-forwarded-for'] || req.connection.remoteAddress));
-    res.send("Hi " + (req.header['x-forwarded-for'] || req.connection.remoteAddress));
+    console.log(`Request recieved from: ${(req.header['x-forwarded-for'] || req.connection.remoteAddress)}`);
+    res.send(`Hi ${(req.header['x-forwarded-for'] || req.connection.remoteAddress)}`);
 });
 
 app.get('/movies', (req, res) => {
     //Get movies
     let gen = req.query.gen;
-    res.send("Getting movies: " + gen);
+    mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            res.send(`Error: ${err}`);
+        }
+
+        console.log("We have succesfully connected");
+
+        let ypdb = db.db('yudonpayMovies');
+        try {
+            ypdb.collection('movies').find({ genre: gen }).toArray((er, data) => {
+                if (err) {
+                    console.log(`Error: ${er}`);
+                    res.send(`Error: ${er}`);
+                }
+                res.send(data);
+            });
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            res.send(`Error: ${error}`);
+        }
+        db.close();
+    });
 });
 
 app.post('/add', (req, res) => {
     //Add movie
     let movie = req.body.movie;
-    res.send("Adding movie: " + JSON.stringify(movie));
+    if (req.body.movie.hasOwnProperty('name') && req.body.movie.hasOwnProperty('director') &&
+        req.body.movie.hasOwnProperty('yearRelease') && req.body.movie.hasOwnProperty('genre')) {
+        movie = req.body.movie;
+    } else {
+        res.send("Error: Movie mising data");
+    }
+    mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            res.send(`Error: ${err}`);
+        }
+
+        console.log("We have succesfully connected");
+
+        let ypdb = db.db('yudonpayMovies');
+        ypdb.collection('movies').insertOne(movie, (er, data) => {
+            if (err) {
+                console.log(`Error: ${er}`);
+                res.send(`Error: ${er}`);
+            }
+            res.send(`Movie created: ${data}`);
+        });
+        db.close();
+    });
 });
 
 app.post('/mod', (req, res) => {
